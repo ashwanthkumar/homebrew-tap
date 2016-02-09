ad41db446c5c4bbf7e9ed5f8ef37bb1721966884
Ashwanth Kumar <ashwanthkumar@googlemail.com>
2016-02-09 14:35:48 +0530

    Upgrading the version of marathonctl to 0.0.3

--- marathonctl.rb
+++ marathonctl.rb
@@ -1,9 +1,9 @@
 class Marathonctl < Formula
   desc "CLI to Mesosphere's Marathon"
   homepage "https://github.com/ashwanthkumar/marathonctl"
-  url "https://github.com/ashwanthkumar/marathonctl/releases/download/v0.0.1/marathonctl-darwin-amd64"
-  version "0.0.1"
-  sha256 "89f86f12f393db400a69434081e116ec3390bceb91ad4d35b204bcfbcd098017"
+  url "https://github.com/ashwanthkumar/marathonctl/releases/download/v0.0.3/marathonctl-darwin-amd64"
+  version "0.0.3"
+  sha256 "3744d719cdf3b34e7e5a4bd644a6f505b429e55572a728353f420c1d2340ddad"
 
   def install
     mv "marathonctl-darwin-amd64", "marathonctl"
